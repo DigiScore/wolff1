d5e6770192c2cda5c541847eade3e08c214202af
Fabrizio Augusto Poltronieri <fabriziopoltronieri@gmail.com>
2025-05-30 13:09:08 +0200

    Simplify position movement logic in draw_xarm module
    Refactored the `move` function to remove redundant checks and calls to `set_position`. This makes the function more concise and ensures a single, straightforward execution path.

--- modules/draw_xarm.py
+++ modules/draw_xarm.py
@@ -333,37 +333,18 @@ class Drawbot(XArmAPI):
         """
         Move to the position (x, y, z) at a given speed and acceleration.
         """
-        self.set_only_check_type(3)
-        code = self.set_position(
-            x=x,
-            y=y,
-            z=z,
-            roll=self.roll,
-            pitch=self.pitch,
-            yaw=self.yaw,
-            speed=speed,
-            mvacc=mvacc,
-            wait=wait,
-            relative=relative,
-            # motion_type=2,
-        )
-        if code == 0:
-            self.set_only_check_type(0)
-            self.set_position(
-                x=x,
-                y=y,
-                z=z,
-                roll=self.roll,
-                pitch=self.pitch,
-                yaw=self.yaw,
-                speed=speed,
-                mvacc=mvacc,
-                wait=wait,
-                relative=relative,
-                # motion_type=2,
-            )
-        else:
-            return
+        self.set_position(x=x,
+                          y=y,
+                          z=z,
+                          roll=self.roll,
+                          pitch=self.pitch,
+                          yaw=self.yaw,
+                          speed=speed,
+                          mvacc=mvacc,
+                          wait=wait,
+                          relative=relative,
+                          # motion_type=2
+                          )
 
     def tool_move(self,
                   abs_angle: int,
